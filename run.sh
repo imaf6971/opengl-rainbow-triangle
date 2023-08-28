e7f01770b7619d373b24a70a9c4b2974bcbcd6c5
Vladislav Mironov <imaf6971@gmail.com>
2023-08-28 16:11:45 +0300

    modified run.sh to run from scratch

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+/usr/bin/cmake --no-warn-unused-cli -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_C_COMPILER:FILEPATH=/usr/bin/gcc -DCMAKE_CXX_COMPILER:FILEPATH=/usr/bin/g++ -S/home/imaf6971/opengl-rainbow-triangle -B/home/imaf6971/opengl-rainbow-triangle/build -G "Unix Makefiles"
 cmake --build ./build --config Debug --target all -j 10 --
 ./build/opengl_project
